194b36ccd26e864c3dced2cc2e5d02be8d525de1
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:54:36 +0100

    test of SQL export

---
+++ Aircraft/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model Aircraft of the subject dataModel.Aeronautics for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE Aircraft_type AS ENUM ('Aircraft');
+CREATE TABLE Aircraft (address json, alternateName text, areaServed text, belongsToAircraftModel text, dataProvider text, dateCreated timestamp, dateIssued timestamp, dateModified timestamp, description text, heading text, id text, isOnGround text, location json, name text, owner json, registration text, seeAlso json, source text, speed text, type Aircraft_type, verticalSpeed text);

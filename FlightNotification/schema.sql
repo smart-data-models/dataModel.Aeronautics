fc47a5d8e2ac93b5e1ce019affc3744d113fa912
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 13:58:45 +0200

    test of SQL export

--- FlightNotification/schema.sql
+++ FlightNotification/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model FlightNotification of the subject dataModel.Aeronautics for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE state_type AS ENUM ('active','inactive','completed','unknown');CREATE TYPE FlightNotification_type AS ENUM ('FlightNotification');
-CREATE TABLE FlightNotification (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateIssued TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, state state_type, type FlightNotification_type);
+CREATE TABLE FlightNotification (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateIssued TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, state state_type, type FlightNotification_type);
